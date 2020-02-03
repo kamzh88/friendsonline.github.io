ad19c9b6cc8aff389221e279e0a037f66018a076
KinKam Zhao <kamzh88@hotmail.com>
2020-02-02 19:45:11 -0500

    an array of all my friends

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
+import AddFriend from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import friends from "./friends.json";
 import "./App.css";
 
-function App() {
-  return (
-    <Wrapper>
-      <h1 className="title">Friends List</h1>
-      <FriendCard
-        name={friends[0].name}
-        image={friends[0].image}
-        occupation={friends[0].occupation}
-        location={friends[0].location}
-      />
-      <FriendCard
-        name={friends[1].name}
-        image={friends[1].image}
-        occupation={friends[1].occupation}
-        location={friends[1].location}
-      />
-      <FriendCard
-        name={friends[2].name}
-        image={friends[2].image}
-        occupation={friends[2].occupation}
-        location={friends[2].location}
-      />
-    </Wrapper>
-  );
+class App extends Component {
+
+  state = {
+    friends
+  };
+
+  removeFriend = id => {
+    // console.log(id);  
+    const friends = this.state.friends.filter(friend => friend.id !== id);
+    this.setState({ friends });
+  };
+
+
+
+  render() {
+    return (
+      <Wrapper>
+        <div className="myfriends">
+          <h1 className="title">Friends List</h1>
+          {this.state.friends.map(friend => (
+            <FriendCard
+              removeFriend={this.removeFriend}
+              id={friend.id}
+              key={friend.id}
+              name={friend.name}
+              image={friend.image}
+              occupation={friend.occupation}
+              location={friend.location}
+            />))}
+        </div>
+        <div className="addfriends">
+          <h1 className="title">Add Friends</h1>
+          {friends.map(friend => (
+            <AddFriend
+              removeFriend={this.removeFriend}
+              id={friend.id}
+              key={friend.id}
+              name={friend.name}
+              image={friend.image}
+              occupation={friend.occupation}
+              location={friend.location}
+            />))}
+        </div>
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -20,9 +20,34 @@ function FriendCard(props) {
           </li>
         </ul>
       </div>
-      <span className="remove">𝘅</span>
+      <span onClick={() => props.removeFriend(props.id)} className="remove" >𝘅</span>
     </div>
   );
 }
 
-export default FriendCard;
+function addFriend(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Occupation:</strong> {props.occupation}
+          </li>
+          <li>
+            <strong>Address:</strong> {props.location}
+          </li>
+        </ul>
+      </div>
+      <span onClick={() => props.removeFriend(props.id)} className="remove" >𝘅</span>
+    </div>
+  );
+}
+
+
+export default (FriendCard, addFriend);
